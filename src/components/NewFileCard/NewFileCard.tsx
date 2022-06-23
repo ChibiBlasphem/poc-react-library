c4eaba23de5d811c472adbf8f95d66fc54ff6102
David Nowinsky <david@nowinsky.net>
2022-06-23 12:16:49 +0200

    File input for NewFileCard

--- src/components/NewFileCard/NewFileCard.tsx
+++ src/components/NewFileCard/NewFileCard.tsx
@@ -1,11 +1,26 @@
 import Card from '../Card/Card';
+import {useRef} from "react";
 
 /**
  * A card to upload a new file.
  */
 export default function NewFileCard({}) {
-    return <Card className="new-file-card" theme="primary">
+    const fileInput = useRef<HTMLInputElement>(null);
+
+    function openFilePicker() {
+        fileInput.current?.click();
+    }
+
+    return <Card className="new-file-card" theme="primary" onClick={openFilePicker}>
         <span className="new-data-file-card__title">Click or drag to add a new file</span><br/>
         <span className="new-data-file-card__subtitle">(XLSX, CSV, JSON, …)</span>
+
+        <input
+            ref={fileInput}
+            className="new-data-file-file-input"
+            type="file"
+            aria-hidden="true"
+            style={{display: 'none'}}
+        />
     </Card>;
 }
